3fd633d82b467d16c48bdaa94838cbdef8836658
Christian <christian.cuero@gmail.com>
2024-10-15 16:48:26 -0400

    se ven las imagenes

--- src/app/gifs/interfaces/gifs.interfaces.ts
+++ src/app/gifs/interfaces/gifs.interfaces.ts
@@ -4,9 +4,8 @@ export interface SearchResponse {
   pagination: Pagination;
 }
 
-export interface Gif
- {
-  type:                       Type;
+export interface Gif {
+  type:                       string;
   id:                         string;
   url:                        string;
   slug:                       string;
@@ -16,9 +15,9 @@ export interface Gif
   username:                   string;
   source:                     string;
   title:                      string;
-  rating:                     Rating;
+  rating:                     string;
   content_url:                string;
-  source_tld:                 SourceTLD;
+  source_tld:                 string;
   source_post_url:            string;
   is_sticker:                 number;
   import_datetime:            Date;
@@ -42,12 +41,42 @@ export interface Onclick {
 
 export interface Images {
   original:                 FixedHeight;
+  downsized:                The480_WStill;
+  downsized_large:          The480_WStill;
+  downsized_medium:         The480_WStill;
+  downsized_small:          DownsizedSmall;
+  downsized_still:          The480_WStill;
   fixed_height:             FixedHeight;
   fixed_height_downsampled: FixedHeight;
   fixed_height_small:       FixedHeight;
+  fixed_height_small_still: The480_WStill;
+  fixed_height_still:       The480_WStill;
   fixed_width:              FixedHeight;
   fixed_width_downsampled:  FixedHeight;
   fixed_width_small:        FixedHeight;
+  fixed_width_small_still:  The480_WStill;
+  fixed_width_still:        The480_WStill;
+  looping:                  Looping;
+  original_still:           The480_WStill;
+  original_mp4:             DownsizedSmall;
+  preview:                  DownsizedSmall;
+  preview_gif:              The480_WStill;
+  preview_webp:             The480_WStill;
+  "480w_still":             The480_WStill;
+}
+
+export interface The480_WStill {
+  height: string;
+  width:  string;
+  size:   string;
+  url:    string;
+}
+
+export interface DownsizedSmall {
+  height:   string;
+  width:    string;
+  mp4_size: string;
+  mp4:      string;
 }
 
 export interface FixedHeight {
@@ -63,18 +92,9 @@ export interface FixedHeight {
   hash?:     string;
 }
 
-export enum Rating {
-  G = "g",
-}
-
-export enum SourceTLD {
-  Empty = "",
-  KiraverseGame = "kiraverse.game",
-  WWWTwitchTv = "www.twitch.tv",
-}
-
-export enum Type {
-  GIF = "gif",
+export interface Looping {
+  mp4_size: string;
+  mp4:      string;
 }
 
 export interface User {
